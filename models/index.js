7fbae2a2f6b3a7b53788248ad1cdf9453f0b5ab7
cpapanicholas <cpapanicholas@gmail.com>
2023-11-25 12:43:32 -0600

    added code to models index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,32 @@
 //add const's for other models the export them.
+
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Associations
+User.hasMany(Post, {
+  foreignKey: 'userId',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+module.exports = { User, Post, Comment };
