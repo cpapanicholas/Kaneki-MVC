c8d4af72d70c6a2d17db265bd6ab07ef653a3f3c
cpapanicholas <cpapanicholas@gmail.com>
2023-11-29 09:57:38 -0600

    front end pathing issues corrected

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,16 +1,78 @@
-const express = require('express');
-const router = express.Router();
-const isAuthenticated = require('../../config/middleware/isAuthenticated');
+// const express = require('express');
+// const router = express.Router();
+// const isAuthenticated = require('../../config/middleware/isAuthenticated');
 
-// Import your models as needed
+// // Import your models as needed
+// const { User } = require('../../models');
+
+// // View user profile (requires authentication)
+// router.get('/profile', isAuthenticated, (req, res) => {
+//   // Render the user profile page with user details
+//   res.render('profile', { user: req.session.user });
+// });
+
+// // Add more user routes (update profile, change password) as needed
+
+// module.exports = router;
+
+const router = require('express').Router();
 const { User } = require('../../models');
 
-// View user profile (requires authentication)
-router.get('/profile', isAuthenticated, (req, res) => {
-  // Render the user profile page with user details
-  res.render('profile', { user: req.session.user });
+router.post('/signup', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-// Add more user routes (update profile, change password) as needed
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -40,7 +40,7 @@ router.get('/', async (req, res) => {
     const projects = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('/home', {
+    res.render('home', {
       projects,
       // logged_in: req.session.logged_in 
     });
@@ -75,7 +75,7 @@ router.get('/post/:id', async (req, res) => {
 
 // ADD WITHAUTH inbetween 'profile', <here> async ...
 // Use withAuth middleware to prevent access to route
-router.get('/profile', async (req, res) => {
+router.get('/dashboard', async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -85,7 +85,7 @@ router.get('/profile', async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
@@ -97,11 +97,21 @@ router.get('/profile', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
 });
 
 
